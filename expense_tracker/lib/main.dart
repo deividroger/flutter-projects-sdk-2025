c160df113cb051a0b7185b49f955b6ef4861ac8f
Deivid Santos <deivid.santos@ayahealthcare.com>
2025-06-13 19:01:16 -0300

    projects created along the course

---
+++ expense_tracker/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+import 'package:expense_tracker/widget/expenses.dart';
+import 'package:flutter/services.dart';
+
+var kColorSchema = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
+
+void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((
+  //   fn,
+  // ) {
+  runApp(
+    MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        brightness: Brightness.dark,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.all(16),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
+      theme: ThemeData().copyWith(
+        colorScheme: kColorSchema,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorSchema.onPrimaryContainer,
+          foregroundColor: kColorSchema.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorSchema.secondaryContainer,
+          margin: const EdgeInsets.all(16),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorSchema.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: kColorSchema.onSecondaryContainer,
+            fontSize: 14,
+          ),
+        ),
+      ),
+      themeMode: ThemeMode.system,
+      home: Expenses(),
+    ),
+  );
+  //});
+}
